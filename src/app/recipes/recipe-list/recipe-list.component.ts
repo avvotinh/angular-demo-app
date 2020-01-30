8417b9fbf210da3a68f4d224d2ac01fabb7edbfd
Le Van Hop <hop.le@axonactive.com>
2020-01-30 18:20:03 +0700

    add shopping editevent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -17,7 +17,7 @@ export class RecipeListComponent implements OnInit {
     ),
     new Recipe(
       "A Test Recipe",
-      "This is simply a test",
+      "This is simply a test 2",
       "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"
     )
   ];
