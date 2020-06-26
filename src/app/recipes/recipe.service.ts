1d1f60b2c150a73a8d588b2c1ffc61dc68cf2910
Le Van Hop <hop.le@axonactive.com>
2020-06-26 15:35:38 +0700

    Implement reactive service with observer

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,10 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
   private recipes: Recipe[] = [
     new Recipe(
       "A Test Recipe",
@@ -19,7 +17,7 @@ export class RecipeService {
       "This is simply a test 2",
       "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg",
       [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
-    )
+    ),
   ];
 
   constructor(private shoppingListService: ShoppingListService) {}
